e14af9dd0c376b2cb63ef640b0f84a5efbc167da
Anton Nizhegorodov <mintobit@gmail.com>
2019-02-02 15:45:40 +0200

    Run setup_web.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ sudo apt-get install -y git vim zsh wget ncdu htop
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 mv zshrc ~/.zshrc
 mv vimrc ~/.vimrc
+./setup_web.sh
 cd ../ && rm -rf env
