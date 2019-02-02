bdac53b67c6f26b136935407ab4508f6b4044776
Anton Nizhegorodov <mintobit@gmail.com>
2019-02-02 16:45:05 +0200

    Add gitconfig

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get install -y git vim zsh wget ncdu htop
 chsh -s /bin/zsh
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+mv gitconfig ~/.gitconfig
 mv zshrc ~/.zshrc
 mv vimrc ~/.vimrc
 ./setup_web.sh
