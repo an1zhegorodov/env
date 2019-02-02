a2c0ff978f0fcb0a6b2c2fee7a3065ab8d2375cb
Anton Nizhegorodov <mintobit@gmail.com>
2019-02-02 17:02:33 +0200

    More robust change of default shell

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install -y git vim zsh wget ncdu htop
-chsh -s /bin/zsh
+chsh -s $(grep /zsh$ /etc/shells | tail -1)
 if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
