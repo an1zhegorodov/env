8c8061ba54c0d52d74c8fbb4fa939b887ccbb3e3
Anton Nizhegorodov <mintobit@gmail.com>
2019-02-02 16:29:37 +0200

    Alternative zsh install

--- setup.sh
+++ setup.sh
@@ -2,10 +2,9 @@
 
 sudo apt-get update
 sudo apt-get install -y git vim zsh wget ncdu htop
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O install_oh-my-zsh.sh
-chmod +x install_oh-my-zsh.sh
-./install_oh-my-zsh.sh
+chsh -s /bin/zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 mv zshrc ~/.zshrc
 mv vimrc ~/.vimrc
 ./setup_web.sh
-cd ../ && rm -rf env
+cd ../ && rm -rf env-develop
