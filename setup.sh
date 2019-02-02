2c1566af988ffeaec42323e1ed41046add0e9dbc
Anton Nizhegorodov <mintobit@gmail.com>
2019-02-02 16:09:06 +0200

    Download oh-my-zsh as file and then execute

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get update
 sudo apt-get install -y git vim zsh wget ncdu htop
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O install_oh-my-zsh.sh
+./intall_oh-my-zsh.sh
 mv zshrc ~/.zshrc
 mv vimrc ~/.vimrc
 ./setup_web.sh
