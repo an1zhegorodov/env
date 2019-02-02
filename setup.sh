b40ae444a61e6197d6a527029cb291597b67cef5
Anton Nizhegorodov <mintobit@gmail.com>
2019-02-02 16:17:10 +0200

    Fix typo && chmod before executing

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 sudo apt-get update
 sudo apt-get install -y git vim zsh wget ncdu htop
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O install_oh-my-zsh.sh
-./intall_oh-my-zsh.sh
+chmod +x install_oh-my-zsh.sh
+./install_oh-my-zsh.sh
 mv zshrc ~/.zshrc
 mv vimrc ~/.vimrc
 ./setup_web.sh
