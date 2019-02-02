3534f2ac3f75f4cdcfebf6cb9c697e6da2517535
Anton Nizhegorodov <mintobit@gmail.com>
2019-02-02 17:14:51 +0200

    Move vhosts dir setup to setup.sh

--- setup.sh
+++ setup.sh
@@ -16,4 +16,32 @@ mv zshrc ~/.zshrc
 echo 'Overriding .vimrc...'
 mv vimrc ~/.vimrc
 echo 'Setting up /var/vhosts folder...'
-./setup_web.sh
+VHOSTS_DIR="/var/vhosts"
+SERVER_GROUP="www-data"
+
+if [ ! -d "$VHOSTS_DIR" ]; then
+    echo "Creating $VHOSTS_DIR"
+    sudo mkdir -p $VHOSTS_DIR
+else
+    echo "Directory $VHOSTS_DIR already exists"
+fi
+
+if [ $(getent group $SERVER_GROUP) ]; then
+    echo "Changing $VHOSTS_DIR ownership to $USER:$SERVER_GROUP"
+    sudo chown $USER:$SERVER_GROUP $VHOSTS_DIR
+else
+    echo "Group $SERVER_GROUP not found"
+    $SERVER_GROUP=$USER
+    echo "Changing $VHOSTS_DIR ownership to $USER:$SERVER_GROUP"
+    sudo chown $USER:$SERVER_GROUP $VHOSTS_DIR
+fi
+echo "Changing $VHOSTS_DIR permissions to 770 g+s"
+sudo chmod 770 $VHOSTS_DIR
+sudo chmod g+s $VHOSTS_DIR
+echo "Setting up rwx ACL permissions for $USER:$SERVER_GROUP"
+sudo setfacl -dm u::rwx $VHOSTS_DIR
+sudo setfacl -dm g::rwx $VHOSTS_DIR
+sudo setfacl -dm u:$USER:rwx $VHOSTS_DIR
+sudo setfacl -dm g:$SERVER_GROUP:rwx $VHOSTS_DIR
+sudo setfacl -m u:$USER:rwx $VHOSTS_DIR
+sudo setfacl -m g:$SERVER_GROUP:rwx $VHOSTS_DIR
